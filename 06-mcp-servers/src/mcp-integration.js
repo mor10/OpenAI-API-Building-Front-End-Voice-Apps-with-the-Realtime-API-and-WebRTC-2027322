df8b3d6124cb71c86dfdc97906a2670ca96a8124
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2025-11-19 20:50:24 -0800

    begin

---
+++ 06-mcp-servers/src/mcp-integration.js
@@ -0,0 +1,11 @@
+export const mcp_servers = [
+  {
+    type: "mcp",
+    server_label: "weather-server",
+    server_description:
+      "A server that provides weather data based on user location or specified location name.",
+    server_url: "https://mcp.stripe.com",
+    authorization: "{access_token}",
+    require_approval: "never",
+  },
+];
