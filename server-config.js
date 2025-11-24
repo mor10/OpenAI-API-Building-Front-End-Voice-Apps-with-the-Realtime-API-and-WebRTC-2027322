43007bd561cba8a485e9fb90b5ed1161cb92f2e1
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2025-11-24 18:27:35 +0000

    cleanup

--- server-config.js
+++ server-config.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = "http://localhost:3000";
+// export const SERVER_URL = "http://localhost:3000";
+export const SERVER_URL = "https://orange-bassoon-wr574gx9j6cv5q9-3000.app.github.dev"
